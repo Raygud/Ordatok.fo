5ae59dc96de0d06dc0ae1b8bed881e345e8473ab
Raygud <78358597+Raygud@users.noreply.github.com>
2021-05-03 13:34:43 +0200

    Create picker.js

---
+++ picker.js
@@ -0,0 +1,385 @@
+function myFunction() {
+  choice = choice.split("\n")
+console.log(choice)
+
+  const random = Math.floor(Math.random() * choice.length);
+  console.log(random, choice[random]);
+  document.getElementById("myText").innerHTML = choice[random];
+}
+
+var choice = `Alinin er av  og sponnin eftir.
+Allir draga sín bát væl omaneftir.
+Allur bati bøtir.
+Alt er hinum svanga søtt.
+Alt sodnar í sveins maga.
+Altíð bagir illum barni okkurt.
+Amari kemur aftan á sparara.
+Ársgrøði er ársføði.
+Ást fjalir lýti.
+Átari goymir sær eftirbita.
+Barnamóðir vil bæði eta<br /> og heim bera.
+Bert er bróðurleyst bak.
+Best er at binda um<br /> heilan fingur.
+Best er heim at aka, <br />meðan vagnurin er heilur.
+Betri er at flýggja <br />enn at falla.
+Betri er at vera góðs <br />mans frilla enn gift illa.
+Betri er at verja borg <br />enn at herja borg.
+Betri er bøtandi hond<br /> enn spillandi tunga.
+Betri er ill hurð fyri <br />gátt enn eingin.
+Betri er ill hurð fyri <br />smáttuni enn eingin.
+Betri er sjálvur at <br />eiga enn bróður at biðja.
+Betri er sund millum <br />vinir og fjørður millum brøður.
+Betri eru smáir fiskar<br /> enn tómir diskar.
+Betri lítið leivt,<br /> enn ovmikið etið.
+Blindar eru ókunnar gøtur.
+Blindur er bókleysur maður.
+Bráð er barna lund.
+Brent barn ræðist eld.
+Bundin er barnamóðir.
+Bundin er bátleysur maður.
+Bøndur skulu bóndum bjóða.
+Deyður ert tú høvuðleysur.
+Djúpt má hann níga, <br />ið kyssa skal hønureyvina.
+Drúgt er tað, ið drýpur.
+Dugandi maður veit <br />sær altíð vákn.
+Dúglig móðir elur <br />lata dóttur.
+Eftir látur kemur grátur.
+Eina ferð lúgvast kúgv í mýri.
+Einar eru seinar hendur.
+Eingin biskupur er <br />ovlærdur og eingin skúladrongur ovbardur.
+Eingin fer so við <br />eikini, at hon ikki flýtur.
+Eingin fer so við <br />trænum, at tað ikki flýtur.
+Eingin kann allar <br />dyr at goyma.
+Eingin kann allar<br /> gáttir røkja.
+Eingin kennir mannin <br />longri enn at tonnunum.
+Eingin kennir mein <br />í annans bein.
+Eingin kvøður betur,<br /> enn hann kann.
+Eingin metir einbýli<br /> við tað, tað er vert.
+Eingin setur inn <br />á einum streymtanga.
+Eingin skal kanna <br />sær bitan, fyrr enn hann er svølgdur.
+Eingin toyggir seg<br /> longri, enn armarnir røkka.
+Eingin veit av søtum at<br /> siga, fyrr enn hann á beiskum bítur.
+Eingin veit á morgni at <br />siga, hvar hann á kvøldi gistir.
+Eingin veit, hvar feigur flakkar.
+Einki er so balt, <br />at tað verður ikki eina ferð alt.
+Einki er so ringt, <br />at tað ikki er gott fyri okkurt.
+Eitil er í øllum feitum.
+Eitt brákar, annað brýtur.
+Eitt eyga er skjótt at bregða.
+Eitt fór í horn, annað í sorn.
+Eitt løstar, og annað brýtur.
+Eitt lúgvar, <br />annað brýtur.
+Eitt toyggir,<br /> annað slítur og triðja brýtur.
+Endurgerð og <br />viðurgerð eru longstir vinir.
+Eyðkendur er <br />úlvur í roði.
+Eydnan hjálpir<br /> tí reysta.
+Eygað er betri <br />enn ársgerðin.
+Eymur er maður,<br /> ið agn sparir.
+Fara upp á tjólegg <br />og detta niður á langlegg.
+Fátt er betri enn<br /> á orði havt.
+Fátt er sum faðir,<br /> einki sum móðir.
+Fátt man feigum forða.
+Fáur kann eitt barn<br /> at eiga.
+Fáur leikar so í sjey<br /> ár sum í sjey dagar.
+Fáur vurðar einbýli tað,<br /> ið tað er vert.
+Feigdin dregur mannin.
+Fingin er fest kona.
+Fjálgur er móður andi.
+Fót setur eingin fyri <br />annan, uttan fallkomin er sjálvur.
+Frá høvdinum skalt tú <br />hvalin skera, og frá kjølinum skalt tú bátin gera.
+Fram kemur hann, ið <br />hóvliga fer.
+Fremmandur fuglur <br />verður altíð happaður.
+Frítt er Eystfelli<br /> frá at fara.
+Frótt er høvur á fullum maga.
+Fult er, áðrenn yvir flýtur.
+Fyrr brestur enn bognar.
+Fyrr er fult, enn yvir flýtur.
+Fyrr trýtur lív enn leistar.
+Fyrr trýtur streymur <br />í á, enn kvinnu trótar ráð.
+Galnir hundar fáa <br />rivið skinn.
+Gamal ravnur er <br />ikki góður at narra.
+Gamal ravnur er <br />illur at narra.
+Ger skálkinum gott,<br /> hann lønir tær aftur við háð og spott.
+Gjørd gerð fær onga vend.
+Gløgt er gestsins eyga.
+Gott er at rista breiðar<br /> álir av annans mans húð.
+Gott er at verja borg,<br /> so leingi eingin søkir.
+Gott er mettum manni at bjóða.
+Gott er óføddum at bæsa.
+Góðar eru fátækra náðir.
+Góðar gløður eftir góðan eld.
+Góður er gamal í ráðum.
+Góðvarin maður kemur <br />seinastur í himmiríki.
+Grør um gangandi fót, <br />heima situr svøltandi kráka.
+Gullið ger lítið uttan<br /> glitrar, matin vil eingin missa.
+Gætt er um eitt eyga.
+Hann doyr ikki, <br />ið dýrt keypir.
+Hann eigur ferðina,<br /> ið fyri ríður.
+Hann eigur reiðina,<br /> ið fyri ríður.
+Hann er ikki óður,<br /> ið ilt ræðist.
+Hann er ikki ræddur, <br />ið ilt ræðist.
+Hann er ikki so haltur, <br />sum hann hinkar.
+Hann er ikki so ræddur<br /> sum illa klæddur.
+Hann fær byr, ið bíðar,<br /> og havn, ið rør.
+Hann fellur, ið feigur er.
+Hann fiskar, ið heim rør.
+Hann grætur ikki eftir <br />gulli, ið ikki hevur sæð tað.
+Hann missir ikki,<br /> ið fyrst fær.
+Hann tekur at sær,<br /> ið sekaður er.
+Hann verður ikki <br />fiskaður við ongum agni.
+Hann, á morgni rýtur,<br /> á kvøldi drítur.
+Hann, ið einki vágar, <br />hann einki vinnur.
+Hann, ið er sekur, <br />inn at sær tekur.
+Hann, ið fer reinur <br />til vás, fer ofta skitin til stás.
+Hann, ið fer uppskrýddur <br />í flór, fer onkuntíð skitin í kór.
+Hann, ið nógvum skal bjóða,<br /> má hava mikið at sjóða.
+Hann, sum býr millum úlvar,<br /> má ýla við teimum.
+Hansara kvørn melur ikki<br /> altíð hveiti.
+Har sum ein skriðan er <br />lopin, er onnur væntandi.
+Har sum tú ert ikki sjálvur, <br />ert tú ikki meiri enn hálvur.
+Har var hond happi hollari.
+Hatta er ikki hveiti <br />á hansara kvørn.
+Heilt er nýtt sár.
+Heima alin er burtur<br /> galin.
+Her er sama tógvið á <br />snælduni.
+Hin ríki traðkar skógvarnar á hol,<br /> men hin fátæki kagar teir á hol.
+Hundar reka annans ørindi.
+Hundur veit húsbóndans vilja.
+Hundurin er sum húsbóndin.
+Hvat skal heiðin hundur<br /> á kirkjugrund?
+Hvat skal hveiti í hunds <br />búk?
+Hvønndagsskrúðir eru ikki <br />hátíðisprúðar.
+Hvør er sínum knúti<br /> kunnigastur.
+Hvør fuglur letur við<br /> sínum nevi.
+Hvør kelling livir<br /> til jóla.
+Hvør rakar brandin<br /> at sínari køku.
+Høgt er heimakeyp.
+Høgt kall gevur høgt fall.
+Ikki eigur at smíða<br /> nøgluna fyrr enn bátin.
+Ikki er alt gull, <br />ið glitrar.
+Ikki er gott at stjala,<br /> har tjóvur er húsbóndi.
+Ikki er hundur ovbardur.
+Ikki fiskast við ongum agni.
+Ikki kemur í kór tað, <br />ið sjálvt vil fara í flór.
+Ikki taka bátin eftir <br />støvnunum ella píkuna eftir hárinum.
+Ikki tekst kúgv <br />av kúleysum manni.
+Illur brandur liggur<br /> longst á eldinum.
+Ilt er at binda hund<br />við smørleyp.
+Ilt er at byggja borð <br />fyri báru.
+Ilt er at læra gamlan<br /> hund uppi at sita.
+Ilt er at skilja latan<br /> drong úr heitari song.
+Ilt er at vera bæði<br /> fátækur og tykkin.
+Ilt er barndømi at kasta.
+Ilt er kynið í kettu,<br /> og so er alt slagið úteftir.
+Ilt er svart skinn<br /> hvítt at tváa.
+Ilt nýtst av bráðræsi.
+Ilt nýtst av illum.
+Jú fleiri hundar,<br /> jú tynri soðið.
+Kallur er tí kollutur, <br />at hann er ofta hárroyttur.
+Kemur fyrst ein danskur maður<br /> í stovuna, koma væl fleiri eftir.
+Knívleysur maður er lívleysur.
+Kráka flýgur best einsamøll.
+Kráku tykir best um unga sín.
+Kúrur kemur eftir kæti <br />og tramin eftir marglæti.
+Køld eru konuráð.
+Latur er hann, <br />ið lognina lastar.
+Lágt má hann lúta, <br />ið høsnareyv skal kyssa.
+Lágt skal hann lúta,<br /> ið hundsrassin skal kyssa.
+Lán skal koma læandi aftur.
+Legg lítt til lítt og<br /> ger tað títt, so verður tað mikið.
+Leingi skal góðum kannast.
+Leingi stendur mansevni til bata.
+Leti ger einki uttan last.
+Leys er kúgv á annans bási.
+Lítið batar at breiða<br /> reint lak á skitna song.
+Lítið hjálpa góð <br />orð við slættibøku.
+Lítið krevur til, <br />at kellingarnev bløðir.
+Lítil byrði er um<br /> langan veg tung.
+Lítil er barnsins uggi.
+Lítil neisti kann<br /> birta stóran eld.
+Livandi maður er<br /> góður at vænta.
+Lóðrið fylgir fóðri.    
+Maður eigur at verða<br /> føddur og ikki gøddur.
+Maður er ikki fjarskotnari <br />frá góðum enn ravnur.
+Maður er mans gaman.
+Maður kemur í mans stað.
+Mangir løkir smáir<br /> gera stórar áir.
+Mangt er dagsins eyga.
+Mangt er dugandi<br /> manni til vápn.
+Mangt er í neyðini nýtt,<br /> sum ikki er í armóðini gitið.
+Mangt man feigum granda.
+Mangur dansar og <br />dansar av neyð.
+Mangur er á orði, <br />ið ikki er á borði.
+Mangur er deyðs mans <br />arvingi og ikki er deyðs mans bróðir.
+Mangur er málvinur,<br /> sum ikki er alvinur.
+Mangur fær tað, ið øðrum er ætlað,<br /> men eingin fær tað, ið øðrum er lagað.
+Mangur fæst við boga, <br />ið er ikki mentur upp at toga.
+Mangur hevur valið og tekur skalið.
+Mangur sigur frá Ólavi <br />kongi og hevur ikki sæð hann.
+Mangur slítur silki av armóð.
+Maturin er undir manningini.
+Meðan tað ringa er,<br /> tá bryggjast tað góða.
+Meira er um skjól<br /> enn um skart.
+Menniskjan kann spáa,<br /> Gud man ráða.
+Mikið má um miklar menn.
+Mong er geitin aðrari lík.
+Nakað er betri enn einki.
+Neyð er ikki nógv breyð.
+Neyð ger nasadjarvan.
+Neyð lærir nakna <br />kvinnu at spinna.
+Neyðin drívur <br />vargin av skóginum.
+Nógv fyri at hoyra<br /> lús hosta og loppu goyggja.
+Nú er alinin av <br />og sponnin eftir.
+Ofta býr fals undir <br />fríðum skinni.
+Ofta býr flagd undir<br />fríðum skinni.
+Ofta eigur svørt ær<br /> hvítt lamb.
+Ofta er ljótur dreymur<br /> fyri lítlum.
+Ofta etur hundur tað, <br />hann havnar, og ketta tað, hon spýr.
+Ofta kemur fjól í <br />bóndans stól og bikkja í bóndans sæti.
+Ofta lýtst happ av hundi.
+Ofta mælir muður,<br /> og fylgir ikki eftir hugur.
+Ofta nýtst ilt av bræði.
+Ofta skríður knørrur<br /> móti forboð.
+Ofta taka trøll góða<br /> manna børn.
+Okkurt fær dugandi<br /> maður til vápn.
+Ódrúgvur er posamatur.
+Ólukkan kemur <br />sjáldan einsumøll.
+Purkan droymir um dravið<br /> og kellingin um stavin.
+Ravnur drepur ikki, <br />har hann byggir.
+Ravnur er feitastur rakár.
+Rignir á prestin, <br />dryppar á deknin.   
+Ring er tann kvørn,<br /> ið mjølið ikki veksur á.
+Ríkir bøndur oksar <br />keypa, mangt má feitt um fátækan leypa.
+Rotið er snart brotið.
+Rødd er hond í annans fati.
+Samt er tógvið á snælduni.
+Sannleikin er illa lýddur.
+Sárt bítur sekan.
+Sárt bítur soltin lús.
+Seigar eru gamlar sinar.
+Seigur er gamal siður.
+Seinar eru einar hendur.
+Sekur at sær tekur.
+Sit vini tínum nær, <br />men sit ikki av hans lær.
+Sjáldan batnar moyggj,<br /> ið heiman fer.
+Sjáldan er berskakin bestur.
+Sjáldan er ein báran støk.
+Sjáldan er síðari betri.
+Sjáldan er trællur <br />í treytum góður.
+Sjáldan gongur roykur av<br /> brandi, og ikki<br /> eldur er í honum.
+Sjáldan hevur góður <br />kvistur sprottið av illum runni.
+Sjáldan kemur dúvuungi úr ravnseggi.
+Sjáldan kemur eitt <br />óløgi einsamalt.
+Sjáldan kemur fluga <br />í feiga manna fat.
+Sjáldan rýkur av <br />ongum brandi.
+Sjáldan sita heimakonur javnar.
+Sjáldan verður forvitin fegin.
+Sjálvgjørt er væl gjørt.
+Skaði ger mannin klókan,<br /> men ikki ríkan.
+Skeri eg teg her, <br />so missi eg teg har.
+Skjótari bera tveir<br /> fuglar í reiður enn ein.
+Skjótt bløðir kellingarnøs.
+Skjótt er at tippa<br /> einar hendur.
+Skjótt er óføddum at bæsa.
+Skjótt er skitið verk.
+Slíkt kann vattarbakið<br /> vera sum skølingurin. 
+Smáir fuglar verpa smá egg.
+Snakidagar koma eftir<br /> bakidagar.
+So argast hvør, sum hann eldist.
+So látast bátar sum skip.
+So mangt er tykki sum maðurin.
+So mætur fuglur er <br />veiddur sum kráka.
+Spottið kemur í spottarans hús<br /> og brennir spottaran inni.
+Sprongd er ikki gongd.
+Stevning fellir ongan mann.
+Sum byttan so lokið.
+Sum maður røkir knívin,<br /> so røkir hann vívið. 
+Sum tú reiðir, skalt tú liggja.
+Svartur verður hann, sum við bik fæst.
+Sær er hond hollast.
+Sær er siður á landi.
+Søtt tykist tí svanga.
+Søtur er sjálvgivin biti.
+Tað bata eingi føgur <br />orð við slættibøku.
+Tað bata so lítið føgur<br /> orð við brugdu.
+Tað blíðkar fyri hugaðum monnum.
+Tað er átari, ið ger mun á eggi.
+Tað er betri at spara á<br /> kjaftinum enn á botninum.
+Tað er eisini eitil í válgara.
+Tað er gott at hava tógva <br />sveinar, tá ið annar er úti, er annar heima.
+Tað er ikki altíð, <br />matbruður stendur opin.
+Tað er ikki hvør dagur bakudagur.
+Tað er ikki hvørt orð, tú skalt svara til,<br /> og ikki hvørt gildi, <br />tú skalt fara til.
+Tað er ikki oftari enn so, at Páll fær røtur.
+Tað er ikki svínum líkt, ið er sínum líkt.
+Tað er mangur leikur í longum talvi.
+Tað er mætari fuglur veiddur enn kráka.
+Tað er ringur fuglur, ið drítur í egið reiður.
+Tað er tvætt í hvørjari ætt.
+Tað er vamm á hvørjum góðgripi.
+Tað grør ikki undir tí steini,<br /> sum altíð verður rassaður.
+Tað hanga ikki allir lyklar<br /> við eitt konubelti.
+Tað lívir ikki meira enn<br /> Ísland lívir Føroyum.
+Tað livir longst, ið øllum<br /> er leiðast.
+Tað skal meira til hestin <br />enn bokslið.
+Tað skjýtst ikki alt, ið upp rennur.
+Tað verður ikki alt fleytir,<br /> ið tyril kemur í.
+Tað, ið illa er yrkt, <br />verður illa kvøðið.
+Tað, ið ungur nemur, <br />hann gamal fremur.
+Tað, sum eygað ikki sær og oyrað ikki hoyrir,<br /> tað virkir ikki hjartað.
+Tað, sum heima er alið,<br /> er burtur galið.
+Tann sum tíðum rør út,<br /> hann fiskar umsíðir.
+Tann, ið Gud vil fløva, <br />batar ongum at køva.
+Tann, sum er úti stongdur, <br />er inni gloymdur.
+Tann, sum fær barnið, fær skarnið.
+Tann, sum ikki etur seg mettan, <br />sleikir seg ikki mettan.
+Tá ið armur kropnast, tá muður opnast.
+Tá ið maður ger, sum hann vil,<br /> ger hann, sum hann er til.
+Tá ið ravnurin legst í fjøruna, <br />veit hann sær lítið.
+Tá ið tann skitni ber vitni,<br /> er illa vorðið.
+Tá ið tvey ynnast, tey væl finnast.
+Tá ið vesalamaður fær vald,<br /> kann hann sær ikki afturhald.
+Tá ið ølið fer inn, fer vitið út.
+Tá veit hin svarti sær lítið,<br /> tá ið hann legst í fjøruna.
+Teir byggja land, ið liva.
+Tey mugu gjalda, sum ikki valda.
+Tí er ikki sætið rótt,<br /> at komin er á banasótt.
+Tí er kallur kollutur,<br /> tí hann er ofta hárruskaður.
+Tíðliga skal krøkjast,<br /> ið góður krókur skal verða.
+Tiga skal millum tveggja vina.
+Tjóvur trýr, at hvør maður stjelur.
+Tolin trívst og treiskin vinnur.
+Tráður lívir trevli.
+Trøllabørnini gráta eftir<br /> hvonnum á jólum.
+Tung er takkarleys gerð.
+Tungt er at hoyra lús hosta <br />og loppu goyggja.
+Tungt er gamla manna fall.
+Tungur er tigandi róður.
+Tunn eru móður oyra.
+Turvin býður mangt.
+Tveir harðir steinar mala<br /> ikki væl.
+Tvey eru eyguni og triði<br /> er muðurin.
+Tyngst slitin er søtasti bitin.
+Tøgn kemur ikki á ting.
+Umsíðir lúgvast kúgv í mýri.
+Ungur nemur, gamal fremur.
+Út kemur innispunnið lín.
+Út kemur innispunnið tógv. 
+Vamm er í hvørjum góðgripi.
+Vandi er í hvørjari vælferð.
+Vani bítur best.
+Vatn kemur aftur, har vatn<br /> hevur verið.
+Vágan vinnur, og vágan tapir.
+Vágin vinnur.
+Vágur millum vinir og fjørður<br /> millum frændur.
+Vánt er at sova hjá sprundi.
+Veðrið blíðkar fyri hugaðum monnum.
+Verstur brandur liggur longst á eldi.
+Vinur er hann, ið vomm sær.
+Vís mær at garði, og eg skal <br />siga tær, hvør inni býr.
+Vís mær mannin og ikki matkeraldið.
+Ymiskt er mansins tykki.
+Ymiss er mansins eydna.
+Øl er annar maður.
+Øllum er ovboðið.`
+myFunction()
